45190f7563cc14a7f8cd3f65ff34a562982153b5
Fredrik Wendt <fredrik@wendt.se>
2010-09-12 15:31:22 +0200

    added sleepUntil(long timeToReturn);

--- src/main/java/se/wendt/util/ThreadUtils.java
+++ src/main/java/se/wendt/util/ThreadUtils.java
@@ -2,16 +2,28 @@ package se.wendt.util;
 
 public class ThreadUtils {
 
+	private static final int MINIMUM_DELAY = 50;
+
 	/**
 	 * This method returns after no less than delay milliseconds.
 	 * 
 	 * @param delay milliseconds to sleep
 	 */
-	public static void sleep(long delay) {
+	public static void sleep(final long delay) {
 		long start = System.currentTimeMillis();
 		long timeToReturn = start + delay;
+		sleepUntil(timeToReturn);
+	}
+	
+	/**
+	 * This method returns no sooner than timeToReturns milliseconds have passed since the epoch.
+	 * 
+	 * @param timeToReturn when this method should return
+	 */
+	public static void sleepUntil(final long timeToReturn) {
 		while (System.currentTimeMillis() < timeToReturn) {
-			delay = Math.max(50, timeToReturn - System.currentTimeMillis());
+			long millisUntilTimeIsUp = timeToReturn - System.currentTimeMillis();
+			long delay = Math.max(MINIMUM_DELAY, millisUntilTimeIsUp);
 			try {
 				Thread.sleep(delay);
 			} catch (InterruptedException e) {
